d1ffd9894ddf1a080be5d2f449dcf1bb22b235dc
Marcus Stancil <stancil.marcus@yahoo.com>
2021-03-21 20:24:35 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,67 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'password-generator';
+  passwordCharacters = ""
+
+  password = "";
+
+  passwordLength: Number;
+
+  containLetters = false;
+
+  containNumbers = false;
+
+  containSymbols = false;
+
+  getButtonActive(){
+    return !(this.password) && !(this.containLetters || this.containNumbers || this.containSymbols)
+  }
+
+  handlePasswordChange(e){
+    this.passwordLength = e.target.value;
+  }
+
+  toggleContainLetters(){
+    this.containLetters = !this.containLetters
+    
+    if (this.containLetters){
+      this.passwordCharacters += "abcdefghijklmnopqrstuvwxyzABCDEFHGIJKLMNOPQRSTUVWXYZ";
+    } else {
+      console.log('replacing')
+      this.passwordCharacters = this.passwordCharacters.replace('abcdefghijklmnopqrstuvwxyzABCDEFHGIJKLMNOPQRSTUVWXYZ', '0')
+    }
+  }
+
+  toggleContainNumbers(){
+    this.containNumbers = !this.containNumbers
+
+    if (this.containNumbers){
+      this.passwordCharacters += "0123456789"
+    } else {
+      this.passwordCharacters = this.passwordCharacters.replace("0123456789", "")
+    }
+  }
+
+  toggleContainSymbols(){
+    this.containSymbols = !this.containSymbols
+
+    if (this.containSymbols){
+      this.passwordCharacters += "!@#$%^&*()_-=+[]{}"
+    } else {
+      this.passwordCharacters = this.passwordCharacters.replace("!@#$%^&*()_-=+[]{}", "")
+    }
+  }
+
+  createPassword(){
+    let tempPassword = []
+
+    for (let i = 0; i < this.passwordLength; i++){
+      let newIndex = Math.floor(Math.random() * this.passwordCharacters.length);
+      tempPassword.push(this.passwordCharacters[newIndex]);
+    }
+
+    this.password = tempPassword.join("");
+  }
 }
